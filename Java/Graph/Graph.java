9e4897539c850c707cac8c2b15daf51fa0cb6e9a
Rane <vinod_rane@nuance.com>
2020-10-06 19:17:57 +0530

    Added DFS and BFS

--- Java/Graph/Graph.java
+++ Java/Graph/Graph.java
@@ -1,6 +1,8 @@
 package graph;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Graph {
 	ArrayList<ArrayList<Integer>> graph;
@@ -25,6 +27,41 @@ public class Graph {
 				System.out.print(" -> " + x);
 			}
 			System.out.println();
+		}
+	}
+
+	public void breadthFirstSearch(int start) {
+		boolean visited[] = new boolean[nodes];
+		Queue<Integer> q = new LinkedList<Integer>();
+		q.add(start);
+		System.out.println("Breadth First Search Traverse");
+		visited[start] = true;
+
+		while (q.isEmpty() == false) {
+			int node = q.poll();
+			System.out.print(node + " ");
+			for (int x : graph.get(node)) {
+				if (visited[x] == false) {
+					visited[x] = true;
+					q.add(x);
+				}
+			}
+		}
+	}
+
+	public void depthFirstSearch(int start) {
+		boolean visited[] = new boolean[nodes];
+		System.out.print("\nDepth First Search Traverse\n");
+		dfsUtil(start, visited);
+	}
+
+	public void dfsUtil(int node, boolean[] visited) {
+		visited[node] = true;
+		System.out.print(node + " ");
+		for (int x : graph.get(node)) {
+			if(visited[x]==false) {
+				dfsUtil(x, visited);
+			}
 		}
 	}
 }
